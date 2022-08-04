88ce64e636b9efe9ce2aa54d191be6ebe2f25901
Segun Ajibola <ajbl.segun@gmail.com>
2022-08-04 14:36:57 +0100

    updateUI

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 let deckId;
+let computerScore = 0;
+let myScore = 0;
 const cardsContainer = document.getElementById("cards");
 const newDeckBtn = document.getElementById("new-deck");
 const drawCardBtn = document.getElementById("draw-cards");
+const header = document.getElementById("header");
+const remainingText = document.getElementById("remaining");
+const computerScoreEl = document.getElementById("computer-score");
+const myScoreEl = document.getElementById("my-score");
 
 function handleClick() {
   fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
     .then((res) => res.json())
     .then((data) => {
+      remainingText.textContent = `Remaining cards: ${data.remaining}`;
       deckId = data.deck_id;
+      console.log(deckId);
     });
 }
 
@@ -17,35 +25,60 @@ drawCardBtn.addEventListener("click", () => {
   fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
     .then((res) => res.json())
     .then((data) => {
+      remainingText.textContent = `Remaining cards: ${data.remaining}`;
       cardsContainer.children[0].innerHTML = `
                 <img src=${data.cards[0].image} class="card" />
             `;
       cardsContainer.children[1].innerHTML = `
                 <img src=${data.cards[1].image} class="card" />
             `;
+      const winnerText = determineCardWinner(data.cards[0], data.cards[1]);
+      header.textContent = winnerText;
+
+      if (data.remaining === 0) {
+        drawCardBtn.disabled = true;
+        if (computerScore > myScore) {
+          // display "The computer won the game!"
+          header.textContent = "The computer won the game!";
+        } else if (myScore > computerScore) {
+          // display "You won the game!"
+          header.textContent = "You won the game!";
+        } else {
+          // display "It's a tie game!"
+          header.textContent = "It's a tie game!";
+        }
+      }
     });
 });
-/**
- * Challenge:
- *
- * Try to determine which of the 2 cards is the "winner" (has higher value)
- * Aces are the card with the highest "score"
- *
- * In parts:
- *
- * 1. Create a function that takes 2 card objects as parameters,
- * `card1` and `card2`. These card objects have a property called
- * `value`, which can be any one of the following strings, in
- * order of rising "score":
- *
- * "2", "3", "4", "5", "6", "7", "8", "9",
- * "10", "JACK", "QUEEN", "KING", "ACE"
- *
- * I.e. "2" is the lowest score and "ACE" is the highest.
- *
- * The function should determine which of the 2 cards (`card1`
- * or `card2`) has the higher score, or if they have the same score.
- *
- * Log which card wins (or "It's a tie!"
- * if they're the same) to the console
- */
+
+function determineCardWinner(card1, card2) {
+  const valueOptions = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "JACK",
+    "QUEEN",
+    "KING",
+    "ACE",
+  ];
+  const card1ValueIndex = valueOptions.indexOf(card1.value);
+  const card2ValueIndex = valueOptions.indexOf(card2.value);
+
+  if (card1ValueIndex > card2ValueIndex) {
+    computerScore++;
+    computerScoreEl.textContent = `Computer score: ${computerScore}`;
+    return "Computer wins!";
+  } else if (card1ValueIndex < card2ValueIndex) {
+    myScore++;
+    myScoreEl.textContent = `My score: ${myScore}`;
+    return "You win!";
+  } else {
+    return "War!";
+  }
+}
